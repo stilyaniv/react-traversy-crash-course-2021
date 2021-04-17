332d2ef961b06eb56f2b3a94c182c53203f9b724
stilyaniv <stennly.mkiv@gmail.com>
2021-04-18 00:51:38 +0100

    Display tasks on home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Header from "./components/Header";
+import Tasks from './components/Tasks'
+import { useState } from "react";
 
 function App() {
+   const [tasks, setTasks] = useState([
+     { id: 1, text: "This", day: "Monday", reminder: true },
+     { id: 2, text: "Other", day: "Friday", reminder: true },
+     { id: 3, text: "Different", day: "Sunday", reminder: false },
+   ]);
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
