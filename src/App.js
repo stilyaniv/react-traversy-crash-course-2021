20a806d13dffc13bd94d9cdf8b0fe632a5001e75
stilyaniv <stennly.mkiv@gmail.com>
2021-04-19 01:13:28 +0100

    Double clicking on tasks toggles reminders

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import Header from "./components/Header";
-import Tasks from './components/Tasks'
+import Tasks from "./components/Tasks";
 import { useState } from "react";
 
 function App() {
-   const [tasks, setTasks] = useState([
-     { id: 1, text: "This", day: "Monday", reminder: true },
-     { id: 2, text: "Other", day: "Friday", reminder: true },
-     { id: 3, text: "Different", day: "Sunday", reminder: false },
-   ]);
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "This", day: "Monday", reminder: true },
+    { id: 2, text: "Other", day: "Friday", reminder: true },
+    { id: 3, text: "Different", day: "Sunday", reminder: false },
+  ]);
 
-   const deleteTask = (id) => {
-     setTasks(tasks.filter((task) => task.id !== id))
-   }
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No tasks to show'}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
