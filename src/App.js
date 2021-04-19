8b047ac5ec549574c6fdd97482099a19cf8885a1
stilyaniv <stennly.mkiv@gmail.com>
2021-04-19 02:10:02 +0100

    Form that adds tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from "react";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     { id: 1, text: "This", day: "Monday", reminder: true },
     { id: 2, text: "Other", day: "Friday", reminder: true },
     { id: 3, text: "Different", day: "Sunday", reminder: false },
   ]);
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -23,7 +31,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
